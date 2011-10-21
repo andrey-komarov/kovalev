2839cfe6405ee9eae9711ce53a8f17c430149447
Andrey Komarov <taukus@gmail.com>
2011-10-21 07:55:12 +0400

    ololo

--- segments/fractions/main.cpp
+++ segments/fractions/main.cpp
@@ -7,7 +7,17 @@ using namespace std;
 
 int main()
 {
-    segment s(point(0, 0), point(1, 0));
-    segment s2(point(0, 1), point(1, 1));
-    cerr << intersects(s, s2) << "\n";
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        if (i % (n / 10) == 0)
+            cerr << "\r" << i << "\n";
+        double x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        segment s1(point(x1, y1), point(x2, y2));
+        cin >> x1 >> y1 >> x2 >> y2;
+        segment s2(point(x1, y1), point(x2, y2));
+        cout << intersects(s1, s2);
+    }
 }
